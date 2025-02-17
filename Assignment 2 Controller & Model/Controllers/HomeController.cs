a9aa2b2dbc93a4f423af8446e6ae1f98efbe10ec
sir-Yozo <donlavapie@gmail.com>
2025-02-17 14:44:20 -0700

    Added Models and bookController

---
+++ Assignment 2 Controller & Model/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_2_Controller___Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public string Index()
+        {
+            return "Home Page";
+        }
+    }
+}
